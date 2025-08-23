eb0104c8be086eb51134d3e5ebb4e8f6da9edf95
mahi131005 <144921697+mahi131005@users.noreply.github.com>
2025-08-23 16:22:24 +0530

    Create dpc_9.cpp

---
+++ dpc_9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+    if (strs.empty()) return "";
+    string prefix = strs[0];
+    for (int i = 1; i < strs.size(); i++) {
+        int j = 0;
+        while (j < prefix.size() && j < strs[i].size() && prefix[j] == strs[i][j]) {
+            j++;
+        }
+        prefix = prefix.substr(0, j);
+        if (prefix.empty()) return "";
+    }
+    return prefix;
+}
+
+int main() {
+    vector<string> strs = {"flower", "flow", "flight"};
+    cout << "Longest Common Prefix: \"" << longestCommonPrefix(strs) << "\"" << endl;
+    return 0;
+}
