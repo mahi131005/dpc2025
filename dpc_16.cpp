b023c01e18d5d20d81a7cdcf26f5dd7061548f7d
mahi131005 <144921697+mahi131005@users.noreply.github.com>
2025-08-30 22:42:05 +0530

    Create dpc_16.cpp
    You are given two integers, N and M. Your task is to find the Least Common Multiple (LCM) of these two numbers. The LCM of two integers is the smallest positive integer that is divisible by both N and M.

---
+++ dpc_16.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long long gcd(long long a, long long b) {
+    while (b != 0) {
+        long long temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+long long lcm(long long a, long long b) {
+    return (a / gcd(a, b)) * b; 
+}
+
+int main() {
+	long long n, m;
+	cout<<"Enter value for n and m: ";
+    cin >> n >> m;
+    cout << lcm(n, m) << endl;
+    return 0;
+}
